c22976684a83a7c3d88725b365fb9e3bdf6ace16
ZHANG Wei <zhangwedu@mm.lvtu.in>
2024-07-25 11:31:29 +0800

    remove github link in Header

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -27,7 +27,7 @@ const showMenu = ref(false)
                 class="w-full h-full rounded-full"
               >
             </span>
-            <span class="mx-2">Sink</span>
+            <span class="mx-2">Shortner</span>
           </a>
         </div>
 
@@ -64,16 +64,6 @@ const showMenu = ref(false)
                 href="/dashboard"
                 title="Sink Dashboard"
               >Dashboard</a>
-              <a
-                href="https://github.com/ccbikai/sink"
-                target="_blank"
-                title="Github"
-                class="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-gray-900 md:px-3 md:w-auto md:rounded-full hover:bg-gray-800 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-gray-800"
-              >
-                <GitHubIcon
-                  class="w-5 h-5 mr-1"
-                />
-                GitHub</a>
               <span class="ml-1">
                 <SwitchTheme />
               </span>
